ae6520d2aaa6d0dcd6e4aabf4bced098d5e0f105
metalnikov <maxummetalnikov@mail.ru>
2023-09-29 21:44:58 +0300

    Был создан класс HotDrinks

---
+++ HotDrinks.java
@@ -0,0 +1,19 @@
+
+public abstract class HotDrinks {
+    protected String name;
+    protected int volume;
+    protected int temperature;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+}
